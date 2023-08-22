b611d81b29de9d4fed2f3855c64dd4f4ef01c824
comcom <comcom@comcomucBookPro.localdomain>
2023-08-22 15:58:32 +0900

    admin

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const Ain = require('@ainblockchain/ain-js').default
 const ain = new Ain('https://testnet-api.ainetwork.ai', 0);
-const privateKey = process.env.PRIVATE_KEY;
+const adminPrivateKey = process.env.PRIVATE_KEY;
 const appName = process.env.APP_NAME;
+const adminAddress = ain.wallet.addAndSetDefaultAccount(adminPrivateKey);
 const app: Express = express();
 app.use(express.json());
 const port = process.env.PORT;
