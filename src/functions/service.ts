412d1f9a401fad8308ca9aeeb8c112ee72178aa7
akaster99 <akaster99@comcom.ai>
2023-11-02 17:25:45 +0900

    back

--- src/functions/service.ts
+++ src/functions/service.ts
@@ -17,9 +17,9 @@ const evaluate = async (value: any) => {
   const client = k8s.KubernetesObjectApi.makeApiClient(kc);
   const specString = await fs.readFile('/home/ubuntu/ainize-tutorial-service/deployment.yaml', 'utf8');
   console.log("input:", value.prompt);
-  specString.replace('$(MODEL)', value.model);
-  specString.replace('$(MODEL_ARGS)', value.model_args);
-  specString.replace('$(TASKS)', value.tasks);
+  specString.replace('\$(MODEL)', value.model);
+  specString.replace('\$(MODEL_ARGS)', value.model_args);
+  specString.replace('\$(TASKS)', value.tasks);
   console.log(specString);
   const specs: k8s.KubernetesObject[] = yaml.loadAll(specString) as k8s.KubernetesObject[];
   const validSpecs = specs.filter((s) => s && s.kind && s.metadata);
