f8239721d978f01758631607273eeb201bb02f50
akaster99 <akaster99@comcom.ai>
2023-11-09 13:53:20 +0900

    param

--- src/functions/service.ts
+++ src/functions/service.ts
@@ -2,7 +2,6 @@ import * as yaml from 'js-yaml';
 import { promises as fs } from 'fs';
 import * as k8s from '@kubernetes/client-node';
 const checkParams = (value: any) => {
-  console.log('value:', value);
   if (typeof value !== 'object') return false;
   // FIXME(yoojin): check required params and fix.
   // Required params.
@@ -61,22 +60,22 @@ const evaluate = async (value: any) => {
 
 const paramStringify = (value: any) => {
   const paramString = 
-    `${value.model ? `--model ${value.model} \\` : '' }` +
-    `${value.model_args ? `--model_args ${value.model_args} \\` : '' }` +
-    `${value.tasks ? `--tasks ${value.tasks} \\` : '' }` +
-    `${value.provid_description ? `--provid_description ${value.provid_description} \\` : '' }` +
-    `${value.num_fewshot ? `--num_fewshot ${value.num_fewshot} \\` : '' }` +
-    `${value.batch_size ? `--batch_size ${value.batch_size} \\` : '' }` +
-    `${value.max_batch_size ? `--max_batch_size ${value.max_batch_size} \\` : '' }` +
-    `${value.device ? `--device ${value.device} \\` : '' }` +
-    `${value.output_path ? `--output_path ${value.output_path} \\` : '' }` +
-    `${value.data_sampling ? `--data_sampling ${value.data_sampling} \\` : '' }` +
-    `${value.no_cache ? `--no_cache ${value.no_cache} \\` : '' }` +
-    `${value.decontamination_ngrams_path ? `--decontamination_ngrams_path ${value.decontamination_ngrams_path} \\` : '' }` +
-    `${value.description_dict_path ? `--description_dict_path ${value.description_dict_path} \\` : '' }` +
-    `${value.check_integrity ? `--check_integrity ${value.check_integrity} \\` : '' }` +
-    `${value.write_out ? `--write_out ${value.write_out} \\` : '' }` +
-    `${value.output_base_path ? `--output_base_path ${value.output_base_path} \\` : '' }`;
+  `${value.model ? `--model ${value.model} ` : '' }` +
+  `${value.model_args ? `--model_args ${value.model_args} ` : '' }` +
+  `${value.tasks ? `--tasks ${value.tasks} ` : '' }` +
+  `${value.provid_description ? `--provid_description ${value.provid_description} ` : '' }` +
+  `${value.num_fewshot ? `--num_fewshot ${value.num_fewshot} ` : '' }` +
+  `${value.batch_size ? `--batch_size ${value.batch_size} ` : '' }` +
+  `${value.max_batch_size ? `--max_batch_size ${value.max_batch_size} ` : '' }` +
+  `${value.device ? `--device ${value.device} ` : '' }` +
+  `${value.output_path ? `--output_path ${value.output_path} ` : '' }` +
+  `${value.data_sampling ? `--data_sampling ${value.data_sampling} ` : '' }` +
+  `${value.no_cache ? `--no_cache ${value.no_cache} ` : '' }` +
+  `${value.decontamination_ngrams_path ? `--decontamination_ngrams_path ${value.decontamination_ngrams_path} ` : '' }` +
+  `${value.description_dict_path ? `--description_dict_path ${value.description_dict_path} ` : '' }` +
+  `${value.check_integrity ? `--check_integrity ${value.check_integrity} ` : '' }` +
+  `${value.write_out ? `--write_out ${value.write_out} ` : '' }` +
+  `${value.output_base_path ? `--output_base_path ${value.output_base_path} ` : '' }`;
   return paramString;
 };
 
