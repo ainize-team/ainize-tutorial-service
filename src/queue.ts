8d1f655f44e4c1174d634d3284674a15d170475c
akaster99 <akaster99@comcom.ai>
2023-11-13 14:59:07 +0900

    shifted data

--- src/queue.ts
+++ src/queue.ts
@@ -32,7 +32,7 @@ export default class Queue {
     finish() {
       console.log("before queue size:", this.size());
       const data = this.queue.shift()!;
-      console.log("shifted data:", data);
+      console.log("shifted data:", data.requestKey);
       if(this.size() > 0) {
         this.run();
       }
