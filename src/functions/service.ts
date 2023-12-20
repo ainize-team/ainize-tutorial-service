d6636e723bcbbd968cace2d776cf173abce180e0
Yoojin Ko <db232100@gmail.com>
2023-12-20 14:54:19 +0900

    feat: update params

--- src/functions/service.ts
+++ src/functions/service.ts
@@ -60,21 +60,25 @@ const evaluate = async (value: any) => {
 const paramStringify = (value: any) => {
   const paramString = 
   `${value.model ? `"--model", "${value.model}"` : '' }` +
-  `${value.model_args ? `, "--model_args", "${value.model_args}"` : '' }` +
+  `${value.model_args ? `, "--model_args", "${value.model_args}",parallelize=True,` : '' }` +
   `${value.tasks ? `, "--tasks", "${value.tasks}"` : '' }` +
-  `${value.provid_description ? `, "--provid_description", "${value.provid_description}"` : '' }` +
   `${value.num_fewshot ? `, "--num_fewshot", "${value.num_fewshot}"` : '' }` +
   `${value.batch_size ? `, "--batch_size", "${value.batch_size}"` : '' }` +
   `${value.max_batch_size ? `, "--max_batch_size", "${value.max_batch_size}"` : '' }` +
   `${value.device ? `, "--device", "${value.device}"` : '' }` +
   `${value.output_path ? `, "--output_path", "${value.output_path}"` : '' }` +
+  `${value.provid_description ? `, "--provid_description", "${value.provid_description}"` : '' }` +
+  `${value.limit ? `, "--limit", "${value.limit}"` : '' }` +
   `${value.data_sampling ? `, "--data_sampling", "${value.data_sampling}"` : '' }` +
-  `${value.no_cache ? `, "--no_cache", "${value.no_cache}"` : '' }` +
+  `${value.use_cache ? `, "--use_cache", "${value.use_cache}"` : '' }` +
   `${value.decontamination_ngrams_path ? `, "--decontamination_ngrams_path", "${value.decontamination_ngrams_path}"` : '' }` +
-  `${value.description_dict_path ? `, "--description_dict_path", "${value.description_dict_path}"` : '' }` +
   `${value.check_integrity ? `, "--check_integrity", "${value.check_integrity}"` : '' }` +
   `${value.write_out ? `, "--write_out", "${value.write_out}"` : '' }` +
-  `${value.output_base_path ? `, "--output_base_path", "${value.output_base_path}"` : '' }`;
+  `${value.log_samples ? `, "--log_samples", "${value.log_samples}"` : '' }` +
+  `${value.show_config ? `, "--show_config", "${value.show_config}"` : '' }`;
+  `${value.include_path ? `, "--include_path", "${value.include_path}"` : '' }`;
+  `${value.gen_kwargs ? `, "--gen_kwargs", "${value.gen_kwargs}"` : '' }`;
+  `${value.verbosity ? `, "--verbosity", "${value.verbosity}"` : '' }`;
   return paramString;
 };
 
